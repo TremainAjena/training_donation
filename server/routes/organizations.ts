ebb3ecba4c267694c58f576575b99e6f7d1bb9b9
Tremain Jones <tremain.jones@USA-MC-221201Y.lan>
2024-08-07 12:58:08 -0500

    added profile to the docker-compose file to re-run the github tests

--- server/routes/organizations.ts
+++ server/routes/organizations.ts
@@ -2,46 +2,36 @@ const express = require('express')
 const { query, validationResult, body } = require('express-validator');
 const validationUtils = require('../utils/validator');
 const router = express.Router()
-const organizationsController = require("../controllers/organizationsController.js");
+const orgController = require("../controllers/orgController.ts");
 
 // GET "/organizations" route:
 // Add Swagger documentation here:
 
-router.route("/").get(organizationsController.getorganizations);
+router.route("/").get(orgController.getOrganizations);
 
 // GET "organizations/:id" route:
 // TODO: fill in Swagger documentation for this route :) 
-router.route("/:id").get(organizationsController.getorganization);
-// router.route("/").post(organizationsController.createorganization);
+router.route("/:id").get(orgController.getOrganization);
+router.route("/").post(orgController.createOrganization);
 
-router.route("/").post([
-    body('name')
-        .isString()
-        .isLength({min: 2})
-        .withMessage('must be at least 2 chars long')
-        .trim(),
-    body('region')
-        .isString()
-        .isLength({min: 3})
-        .withMessage('must be at least 3 chars long')
-        .trim(),
-   
-    //  This is for custom error messages
-    // .custom(async(value, {req}) => {
-    //         const customer = await prisma.customers.findFirst({where: {first_name: value}});  *Consider separating prisma out into a service object for cleaner code
-    //         console.log(customer)
-    //         if (customer) {
-    //             throw new Error('First name exceeds character limit.');
-    //         }
-    //         return true
-    // })
-],
-validationUtils.validate,
-organizationsController.createorganization
-);
+// router.route("/").post([
+//     // body('name')
+//     //     .isString()
+//     //     .isLength({min: 2})
+//     //     .withMessage('must be at least 2 chars long')
+//     //     .trim(),
+//     // body('region')
+//     //     .isString()
+//     //     .isLength({min: 3})
+//     //     .withMessage('must be at least 3 chars long')
+//     //     .trim(),
+// ],
+// validationUtils.validate,
+// orgController.createOrganization
+// );
 
-router.route("/:id").put(organizationsController.updateorganization);
-router.route("/:id").delete(organizationsController.deleteorganization);
+router.route("/:id").put(orgController.updateOrganization);
+router.route("/:id").delete(orgController.deleteOrganization);
 
 
 
