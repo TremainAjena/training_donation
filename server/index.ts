1484b064abd214f771c2ddfed0ee117d38409b79
Tremain Jones <tremain.jones@USA-MC-221201Y.lan>
2024-08-19 15:46:06 -0500

    Corrected the seed data for events to work with the docker containers and now have working global integration tests.

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 //Importing Libraries
 import { app } from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
